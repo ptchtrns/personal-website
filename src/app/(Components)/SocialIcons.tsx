063ec511c8b2b9d3b0b7f450be5b994d04cc1558
ptchtrns <ptchtrns@gmail.com>
2024-02-19 17:48:43 +0200

    added one more social link

--- src/app/(Components)/SocialIcons.tsx
+++ src/app/(Components)/SocialIcons.tsx
@@ -6,6 +6,7 @@ export default function SocialIcons () {
     <>
       <Link href="https://github.com/ptchtrns" target="_blank" className="inline-block opacity-80 hover:opacity-90 mr-2"><Image src="/images/github.svg" height={23} width={23} alt="Github icon" className="dark:invert" /></Link>
       <Link href="https://www.reddit.com/user/ptchtrns" target="_blank" className="inline-block opacity-80 hover:opacity-90 mr-2"><Image src="/images/reddit.svg" height={23} width={23} alt="Reddit icon" className="dark:invert" /></Link>
+      <Link href="https://www.fiverr.com/ptchtrns/design-and-code-a-website" target="_blank" className="inline-block opacity-80 hover:opacity-90 mr-2"><Image src="/images/fiverr.svg" height={23} width={23} alt="Fiverr icon" className="dark:invert" /></Link>
     </>
   )
 }
